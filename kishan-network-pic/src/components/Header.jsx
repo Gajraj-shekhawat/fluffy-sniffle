ece19302a3f0ae19e21353bc51498e4d738f4400
Gajraj Singh Shekhawat <gaju2gajraj@gmail.com>
2022-06-18 22:18:53 +0530

    Header DOne

---
+++ kishan-network-pic/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './styles/header.module.css';
+const Header = () => {
+  return (
+    <div>
+      <div className={styles.heading}>
+        <div className={styles.logo}>
+          <h3>OYODO</h3>
+        </div>
+        <div className={styles.heading_options}>
+          <h5>HOME</h5>
+          <h5>DAPIBUS</h5>
+          <h5>ULTRICIES</h5>
+          <h5>BIBENDUM</h5>
+          <h5>MAGMA</h5>
+        </div>
+      </div>
+      <div>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+          debitis fugiat susc
+        </span>
+        <br />
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        </span>
+        <br />
+        <span>Lorem ipsum dolor sit amet consecte</span>
+          </div>
+         
+    </div>
+  );
+};
+
+export default Header;
