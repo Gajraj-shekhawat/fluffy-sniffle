d129c92840067cede44ffca11eb5490258d207fa
Gajraj Singh Shekhawat <gaju2gajraj@gmail.com>
2022-06-19 14:08:14 +0530

    Updated

--- kishan-network-pic/src/App.js
+++ kishan-network-pic/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
+import MiddleArea from "./components/MiddleArea";
 
 
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <MiddleArea/>
     </>
   );
 }
---
+++ kishan-network-pic/src/components/MiddleArea.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './styles/middlearea.module.css';
+
+const MiddleArea = () => {
+  return (
+    <div className={styles.container}>
+      <div>
+        {/* <h2>Justo Vulputate Torter Sem</h2> */}
+        <h4>
+          Lorem ips, nesadipisicing elit. Facere mollitia et quo
+          enim eum, nihil quae totam veritatis harum! Totam illum earum id,
+          dicta minima voluptates officiis ducimus omnis ut! tione ipsum
+          voluptatem ut pariatur ullam nisiluta arciquid aliquam? Lorem ipsum
+          dolor sit amet, conse!
+        </h4>
+      </div>
+      <div className={styles.aa}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quas
+        aliquid molestias earum aliquam natus voluptatem tempore aut rerum animi
+        expedita id sapiente facere, quaerat laboriosam commodi dolor quae fuga.
+              <h2>uigguigugig l</h2>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga impedit quis neque voluptate provident animi odit dolore in libero vel architecto esse quae doloremque placeat dolores, soluta explicabo minus sequi!
+              Labore officiis exercitationem asperiores ipsam natus dolorum nam rerum quas sapiente ab aspernatur, quae laborum nobis nesciunt ducimus nulla voluptatum commodi expedita, qui architecto, facilis eligendi? Atque voluptates delectus beatae.
+              Soluta veniam nulla laborum iusto officiis eum facilis voluptas rerum, iste minus quae molestiae, nihil similique sequi praesentium qui consequuntur itaque quas incidunt consectetur neque commodi exercitationem ex repellendus! Dolor.
+              Fugiat alias omnis a velit ea possimus assumenda sed amet laboriosam temporibus? Rem eaque reiciendis unde quas illum id voluptatem perferendis atque, suscipit, fugiat esse dignissimos molestiae eligendi quisquam iste?
+              Placeat exercitationem veritatis facere voluptatem in illum voluptatum consequatur dolore labore, fugiat expedita repudiandae quae dolores? Reiciendis similique voluptatum ipsam porro. Fugiat eos, repellat exercitationem adipisci deleniti mollitia ea! Hic?
+              Aut dolorum reprehenderit distinctio fuga animi! Voluptate, eius facere asperiores possimus corrupti consectetur fugiat distinctio obcaecati sed alias vel ducimus tempora dolorem laborum dicta sequi nisi, quasi molestias? Dolores, labore.
+              Odit inventore eos quidem blanditiis ipsa corrupti aspernatur? Tempore, blanditiis ex! A, quisquam accusantium blanditiis odit officia facere deleniti ipsum reiciendis? Possimus cumque voluptas odit aspernatur voluptatem porro ducimus quaerat!
+              A, omnis totam officia amet optio hic ipsum harum? Amet, commodi repellat? Enim ipsum soluta ab voluptate quam qui tempore, officia cumque debitis corrupti libero adipisci quibusdam cupiditate possimus nobis!
+              Harum vero, cupiditate repellendus delectus earum sapiente itaque vitae similique libero iste illum dolorem. Quisquam dolor, mollitia exercitationem dolores voluptatibus eveniet at similique voluptas dolorum adipisci nesciunt temporibus neque architecto.
+              Id tenetur ipsam commodi esse amet consequatur maiores doloremque qui perferendis dolore, excepturi reprehenderit rem odio delectus maxime iure ullam? Quisquam distinctio accusantium nihil doloremque rem et quis impedit autem.
+      </div>
+    
+    </div>
+  );
+};
+
+export default MiddleArea;
